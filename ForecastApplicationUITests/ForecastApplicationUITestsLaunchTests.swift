740de30d5b5a49cdde2a888f20b49275854d31b6
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-08-05 09:53:43 +0300

    Initial Commit

---
+++ ForecastApplicationUITests/ForecastApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ForecastApplicationUITestsLaunchTests.swift
+//  ForecastApplicationUITests
+//
+//  Created by Hüdahan Altun on 5.08.2023.
+//
+
+import XCTest
+
+final class ForecastApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
